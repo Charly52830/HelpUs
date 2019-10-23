5147d9c8169b32d9c5df0f40c5f7c9f3dae6a118
vagrant <vagrant@homestead>
2019-10-23 15:35:54 +0000

    Agregados modelo y controlador de publicaciones

---
+++ HelpUs/database/migrations/2019_10_23_153124_crear_tabla_publicacion.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaPublicacion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publicacion', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('titulo',64);
+            $table->string('contenido',512);
+            $table->string('link_img',256);
+            $table->date('fecha_pub');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publicacion');
+    }
+}

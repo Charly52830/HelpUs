41e80dd4124e315d376a1cf4c87cf1ce8f24cdfc
jose zorrilla <29105138@uaz.edu.mx>
2019-10-25 08:18:54 -0500

    se agregan imagenes de los acosos y se modifica la vista de index

--- HelpUs/resources/views/pages/index.blade.php
+++ HelpUs/resources/views/pages/index.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.base')
 @section('content')
-	<div class="jumbotron">Este es un jumbotron</div>
-	<h1>Acosos</h1>
+    <div class="jumbotron">Este es un jumbotron</div>
+    <h1>Acosos</h1>
     <div class="row align-items-center">
         @foreach($acosos as $ac)
-        <div class="col-6 col-md-4">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{$ac->nombre}}</h5>
-                    <p class="card-text">{{$ac->resumen}}</p>
+            <div class="col-6 col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('img/'. $ac->imagen) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$ac->nombre}}</h5>
+                        <p class="card-text">{{$ac->resumen}}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{route('acoso.detalle', $ac) }}" type="button" class="btn btn-secondary btn-lg btn-block">Saber más</a>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <a href="{{route('acoso.detalle', $ac) }}" type="button" class="btn btn-secondary btn-lg btn-block">Saber más</a>
-                </div>
+                </br>
             </div>
-            <br>
-        </div>
-        <br>
+            @if ($loop->iteration % 3 == 0)
+    </div>
+    <br>
+    <br>
+    <div class="row">
+        @endif
+
         @endforeach
     </div>
 @stop

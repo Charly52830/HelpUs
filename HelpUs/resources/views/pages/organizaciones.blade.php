8d944098dcd8693a3e37f1c478646771327accf0
Kidd <angel.kidd26@gmail.com>
2019-10-29 11:13:11 -0600

    cambio en el tamaño de la card y link de la pagina

--- HelpUs/resources/views/pages/organizaciones.blade.php
+++ HelpUs/resources/views/pages/organizaciones.blade.php
@@ -5,13 +5,14 @@
 	<div class="row">
 		@foreach($org as $orgs)
 	    	<div class="col-sm">
-	    		<div class="card" style="width: 18rem;">
+	    		<div class="card" style="width: 20rem;">
 	    			<img class="card-img-top" src="img/{{$orgs->imagen}}" alt="">
 				  <div class="card-body">
 				    <h5 class="card-title">{{$orgs->nombre}}</h5>
 				    <p class="card-text">{{$orgs->resumen}}</p>
-				    <p class="card-text">Teléfono: {{$orgs->telefono}}</p>
-				    <a href="#" class="btn btn-primary">ver más...</a>
+                    <p class="card-text">Teléfono: {{$orgs->telefono}}</p>
+                   
+				    <a href="{{$orgs->enlace}}" class="btn btn-primary">ver más...</a>
 				  </div>
 				</div>
 	    	</div>

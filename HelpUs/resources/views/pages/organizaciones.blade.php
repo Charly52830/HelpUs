43fe9b3b87f74e3ccc4560cc521c593a99cf4c00
Kidd <angel.kidd26@gmail.com>
2019-11-05 11:56:12 -0600

    Se modifica la interfaz de organizaciones

--- HelpUs/resources/views/pages/organizaciones.blade.php
+++ HelpUs/resources/views/pages/organizaciones.blade.php
@@ -5,18 +5,23 @@
 	<h1>Organizaciones</h1>
 	<div class="row" >
 		@foreach($org as $orgs)
-	    	<div class="col">
-	    		<div class="card" style="width: 20rem;">
+	    	<div class="col-md-4 col-lg-4 d-flex align-items-stretch">
+	    		<div class="card">
 	    			<img class="card-img-top" src="img/{{$orgs->imagen}}" width="100px" height="250px">
-				  <div class="card-body">
+				  	<div class="card-body">
 				    <h5 class="card-title">{{$orgs->nombre}}</h5>
 				    <p class="card-text">{{$orgs->resumen}}</p>
                     <p class="card-text">Teléfono: {{$orgs->telefono}}</p>
-                   
-				    <a href="{{$orgs->enlace}}" class="btn btn-primary">ver más...</a>
+				    <a href="{{$orgs->enlace}}" class="btn btn-nuevo btn-lg btn-block">ver más...</a>
 				  </div>
 				</div>
-	    	</div>
-        @endforeach
+			</div>
+			@if ($loop->iteration % 3 == 0)
+    			</div>
+    				<br>
+    				<br>
+    			<div class="row">
+        	@endif
+		@endforeach
     </div>
 @endsection

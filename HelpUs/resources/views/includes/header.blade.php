dde717668fbc91b6aeac4559befbd6f3d4c57946
Charly52830 <g.m.52830A@gmail.com>
2019-11-14 07:15:59 -0600

    Corregidos aspectos de imagen y cartas responsivas

--- HelpUs/resources/views/includes/header.blade.php
+++ HelpUs/resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="{{route('acoso.informacionGeneral')}}">
-		<img src="{{ asset('img/Logo.png') }}" alt="HelpUs logo" class="">
+<nav class="navbar navbar-expand-md navbar-light">
+	<a class="navbar-brand" href="{{ route('acoso.informacionGeneral') }}">
+		<img src="{{ asset('img/Logo.png') }}" alt="HelpUs logo" class="img-solid">
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse"
 		data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,22 +9,16 @@
 	<div class="collapse navbar-collapse" id="navbarNav">
 		<ul class="navbar-nav">
 			<li class="nav-item">
-				<a class="nav-link" href="{{route('acoso.informacionGeneral')}}">Home<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="{{ route('acoso.informacionGeneral') }}">Home<span class="sr-only"></span></a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="{{route('publicaciones.show_all')}}">Foro</a>
+				<a class="nav-link" href="{{ route('publicaciones.show_all') }}">Foro</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="{{route('organizaciones.infoGeneral')}}">Organizaciones</a>
+				<a class="nav-link" href="{{ route('organizaciones.infoGeneral') }}">Organizaciones</a>
 			</li>
 		</ul>
 	</div>
 </nav>
-<hr width="100%">
-<div class="align-middle">
-	<a class="text-center" href="{{route('acoso.informacionGeneral')}}">
-		<img class="rounded mx-auto d-block" src="{{ asset('img/Logo.png') }}" alt="HelpUs logo" class="">
-	</a>
-	<div class="text-center text-mute font-weight-light font-italic">Sitio de información sobre el acoso</div>
-</div>
-<hr width="100%">
+<hr>
+
--- HelpUs/resources/views/pages/organizaciones.blade.php
+++ HelpUs/resources/views/pages/organizaciones.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.base')
-
-
 @section('content')
-	<div class="row">
+	<h1>Organizaciones</h1>
+	<br>
+	<div class="card-deck">
 		@foreach($org as $orgs)
-	    	<div class="col-sm">
-	    		<div class="card" style="width: 20rem;">
-	    			<img class="card-img-top" src="img/{{$orgs->imagen}}" width="100" height="250" alt="">
-				  <div class="card-body">
-				    <h5 class="card-title">{{$orgs->nombre}}</h5>
-				    <p class="card-text helpus-font">{{$orgs->resumen}}</p>
-                    <p class="card-text">Teléfono: {{$orgs->telefono}}</p>
-                   
-				    <a href="{{$orgs->enlace}}" class="btn btn-nuevo">ver más...</a>
-				  </div>
+			<div class="card 
+				@if ($loop->iteration % 2 == 0)
+					bg-helpus">
+				@else
+					bg-helpus2">
+				@endif
+				<img class="card-img-top img-card"  src="img/{{ $orgs->imagen }}"  alt="">
+				<div class="card-body">
+					<h5 class="card-title">{{ $orgs->nombre }}</h5>
+					<hr>
+					<p class="card-text text-justify helpus-font">{{ $orgs->resumen }}</p>
+					<p class="card-text">Teléfono: {{ $orgs->telefono }}</p>
 				</div>
-	    	</div>
-        @endforeach
-    </div>
+				<div class="card-footer">
+					<a href="{{ $orgs->enlace }}" class="btn btn-nuevo">Saber más</a>
+				</div>
+			</div>
+			@if($loop->iteration % 3 == 0)
+				</div>
+				<br>
+				<div class="card-deck">
+			@endif
+		@endforeach
+	</div>
 @endsection

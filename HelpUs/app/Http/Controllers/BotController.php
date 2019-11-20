6f28773dee8762de667a57667010497f064d4b4f
lfga98 <lfgarciauaz@gmail.com>
2019-11-20 09:37:45 -0600

    Ahora el bot se conecta al servicio en linea

--- HelpUs/app/Http/Controllers/BotController.php
+++ HelpUs/app/Http/Controllers/BotController.php
@@ -7,44 +7,38 @@ use GuzzleHttp\Client;
 
 class BotController extends Controller
 {
-
+  
   private $mensajes = array();
   public function chat(){
-
     if (isset($_COOKIE['botsesion'])) {
       $id=$_COOKIE['botsesion'];
-      echo "destrui sesion ".$id;
       $client = new Client();
-      $response = $client->request('GET', 'http://localhost:8080/sesion?sessionId='.$id);
+      $response = $client->request('GET', 'http://148.72.65.115:8080/servicio/sesion?sessionId='.$id);
       setcookie("botsesion", "", time() - 3600);
 
     }
     $client = new Client();
-    $response = $client->request('GET', 'http://localhost:8080/chat');
+    $response = $client->request('GET', 'http://148.72.65.115:8080/servicio/chat');
     $statusCode = $response->getStatusCode();
     $body = $response->getBody();
     $arregloRespuesta = json_decode($body,true);
     $idsesion= $arregloRespuesta["mensaje"];
     setcookie('botsesion',$idsesion);
     $res= $arregloRespuesta["respuesta"];
-    echo "cree sesion".$idsesion;
     return view('pages/chat',['mensajes'=>$res,'sessionid'=>$idsesion]);
   }
 
 
   public function respuesta(Request $request)
   {
-    	$client = new Client();
-
-    	$response = $client->request('GET', 'http://localhost:8080/message?mensaje='.$request->pregunta.'&sessionId='.$request->idsesion);
-
-    	$statusCode = $response->getStatusCode();
-    	$body = $response->getBody();
-
-      $arregloRespuesta = json_decode($body,true);
-      array_push($this->mensajes,$arregloRespuesta["mensaje"],$res= $arregloRespuesta["respuesta"] );
-      return view('pages/respuestas',['mensajes'=>$this->mensajes,'sessionid'=>$request->idsesion]);
-    }
+    $client = new Client();
+    $response = $client->request('GET', 'http://148.72.65.115:8080/servicio/message?mensaje='.$request->pregunta.'&sessionId='.$request->idsesion);
+    $statusCode = $response->getStatusCode();
+    $body = $response->getBody();
+    $arregloRespuesta = json_decode($body,true);
+    array_push($this->mensajes,$arregloRespuesta["mensaje"],$res= $arregloRespuesta["respuesta"] );
+    return view('pages/respuestas',['mensajes'=>$this->mensajes,'sessionid'=>$request->idsesion]);
+  }
 
 
 
--- HelpUs/resources/views/pages/chat.blade.php
+++ HelpUs/resources/views/pages/chat.blade.php
@@ -1,47 +1,47 @@
 @extends('layouts.base')
 @section('content')
 <div class="card grey lighten-3 chat-room">
-<div class="card-body">
+  <div class="card-body">
 
 
-  <div class="row px-lg-2 px-2">
-    <div class="col-md-12 pl-md-3 px-lg-auto px-0">
-      <div class="chat-message">
+    <div class="row px-lg-2 px-2">
+      <div class="col-md-12 pl-md-3 px-lg-auto px-0">
+        <div class="chat-message">
 
-        <ul class="list-unstyled chat">
-          <li class="d-flex justify-content-between mb-4">
-            <div class="chat-body white p-3  z-depth-1">
-              <div class="header">
-                <strong class="primary-font">Help Us Bot</strong>
+          <ul class="list-unstyled chat">
+            <li class="d-flex justify-content-between mb-4">
+              <div class="chat-body white p-3  z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Help Us Bot</strong>
+                </div>
+                <hr class="">
+                <p class="mb-0">
+                  {{$mensajes}}
+                </p>
               </div>
-              <hr class="">
-              <p class="mb-0">
-                {{$mensajes}}
-              </p>
-            </div>
-          </li>
+            </li>
 
-          <li class="white">
-          <form action="{{route('bot.respuesta')}}" method="post">
-            @csrf
-              <div class="form-group basic-textarea">
-                <textarea class="form-control pl-2 my-0" rows="3" placeholder="Escribe tu mensaje aquí" name="pregunta" id="pregunta"></textarea>
-                <input type="hidden" name="idsesion" value="{{$sessionid}}"
-								id="idsesion"  >
-              </div>
-            <button type="submit"  class="btn btn-info btn-rounded btn-sm waves-effect waves-light float-right">Enviar</button>
-          </form>
-          </li>
+            <li class="white">
+              <form action="{{route('bot.respuesta')}}" method="post">
+                @csrf
+                <div class="form-group basic-textarea">
+                  <textarea class="form-control pl-2 my-0" rows="3" placeholder="Escribe tu mensaje aquí" name="pregunta" id="pregunta"></textarea>
+                  <input type="hidden" name="idsesion" value="{{$sessionid}}"
+                  id="idsesion"  >
+                </div>
+                <button type="submit"  class="btn btn-info btn-rounded btn-sm waves-effect waves-light float-right">Enviar</button>
+              </form>
+            </li>
 
-        </ul>
+          </ul>
+
+        </div>
 
       </div>
 
+
     </div>
 
-
   </div>
-
-</div>
 </div>
 @stop

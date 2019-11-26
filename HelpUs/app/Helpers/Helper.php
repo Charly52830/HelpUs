907d1c397b0a8b6b2eb504b30aa726adda847a8c
Charly52830 <g.m.52830A@gmail.com>
2019-11-26 00:57:17 -0600

    Agreadas frases y palabras con las que se entrena Watson. Ahora se maneja de forma adecuada la sesión de Watson con las cookies del cliente

--- HelpUs/app/Helpers/Helper.php
+++ HelpUs/app/Helpers/Helper.php
@@ -2,15 +2,43 @@
 
 /**
  *	Autor: Carlos Ruvalcaba
- *	Fecha: 24/11/2019
+ *	Fecha: 25/11/2019
  *
  *	Fuentes:
  *	https://tutsforweb.com/creating-helpers-laravel/
- *	https://weichie.com/blog/curl-api-calls-with-php/
  * */
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+
+if (!function_exists('verifica_sesion')) {
+	 
+	 /*
+	  * Función que verifica si existe una sesión activa del bot. Si no existe
+	  * entonces crea una nueva sesión y fija la expiración a 5 minutos, si
+	  * existe entonces actualiza la expiración a 5 minutos adelante del tiempo
+	  * actual del servidor.
+	  * 
+	  * El identificador de la sesión de Watson es guardado en forma de cookie.
+	  * Trata de simular el control de inactividad de Watson.
+	  * */
+	 function verifica_sesion() {
+	 	if(!isset($_COOKIE['botsesion'])) {
+			$client = new Client();
+			$response = $client->request('GET', 'http://148.72.65.115:8080/servicio/chat');
+			//$statusCode = $response->getStatusCode();
+			$body = $response->getBody();
+			$arregloRespuesta = json_decode($body,true);
+			$id_sesion = $arregloRespuesta["mensaje"];
+			setcookie('botsesion', $id_sesion, time()+300);
+		}
+		else {	// Reinicia el tiempo de inactividad
+			$id_sesion = $_COOKIE['botsesion'];
+			setcookie('botsesion', $id_sesion, time()+300);
+		}
+	 }
+}
+
 
 if (!function_exists('start_bot_session')) {
 
@@ -18,18 +46,10 @@ if (!function_exists('start_bot_session')) {
 	 * Inicia una nueva conversación con el HelpUsBot. La sesión es inicializada
 	 * a través de una solicitud POST al web service de Watson.
 	 *
-	 * @return string Cadena que contiene el identificador de la sesión iniciada.
+	 * @return string Vista con el chat del bot.
 	 * */
 	function start_bot_session() {
-		if(!isset($_COOKIE['botsesion'])) {
-			$client = new Client();
-			$response = $client->request('GET', 'http://148.72.65.115:8080/servicio/chat');
-			//$statusCode = $response->getStatusCode();
-			$body = $response->getBody();
-			$arregloRespuesta = json_decode($body,true);
-			$idsesion = $arregloRespuesta["mensaje"];
-			setcookie('botsesion', $idsesion, time()+3000);
-		}
+		verifica_sesion();
 		return View::make('layouts.bot');
 	}
 }
